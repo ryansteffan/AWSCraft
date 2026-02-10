ae0e6b8a68fb9f5cb7cc0889006495a1190d3da4
Ryan Steffan <ryansteffan.biz@gmail.com>
2026-02-09 20:53:07 -0600

    added support for setting up the minecraft server on the instance

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -592,45 +592,11 @@ resource "aws_instance" "MinecraftServerInstance" {
   user_data = templatefile(
     "user_data.sh",
     {
-      s3_bucket          = aws_s3_bucket.MinecraftData.bucket,
-      service_key        = aws_s3_object.MinecraftStartServerServiceObject.key,
-      server_profile_key = aws_s3_object.MinecraftServerProfile.key,
+      s3_bucket                = aws_s3_bucket.MinecraftData.bucket,
+      start_server_service     = aws_s3_object.MinecraftStartServerServiceObject.key,
+      minecraft_server_profile = aws_s3_object.MinecraftServerProfile.key,
     }
   )
-
-  # user_data     = <<-EOF
-  #               #!/bin/bash
-
-  #               # Update and install necessary packages
-  #               apt-get update -y
-  #               apt-get install -y awscli unzip python3
-
-  #               # Pull the start script from S3
-  #               aws s3 cp s3://${aws_s3_bucket.MinecraftData.bucket}/scripts/start-server.sh /home/ubuntu/start-server.sh
-  #               chmod +x /home/ubuntu/start-server.sh
-
-  #               # Pull the stop script from S3
-  #               aws s3 cp s3://${aws_s3_bucket.MinecraftData.bucket}/scripts/stop-server.py /home/ubuntu/stop-server.py
-  #               chmod +x /home/ubuntu/stop-server.py
-
-  #               # Pull the start service from S3
-  #               aws s3 cp s3://${aws_s3_bucket.MinecraftData.bucket}/services/${aws_s3_object.MinecraftStartServerServiceObject.key} /etc/systemd/system/start-server.service
-
-  #               # Enable the services to run at startup
-  #               systemctl enable start-server.service
-
-  #               # Download the Minecraft Profile from S3
-  #               aws s3 cp s3://${aws_s3_bucket.MinecraftData.bucket}/profiles/${aws_s3_object.MinecraftServerProfileObject.key} /opt/minecraft_server_profiles/
-
-  #               # Setup the profile
-  #               unzip /opt/minecraft_server_profiles/${aws_s3_object.MinecraftServerProfileObject.key} \ 
-  #               -d ${replace("/opt/minecraft_servers/${aws_s3_object.MinecraftServerProfileObject.key}", ".zip", "")}
-
-  #               # Run the java install script
-  #               chmod +x /opt/minecraft_servers/${replace(aws_s3_object.MinecraftServerProfileObject.key, ".zip", "")}/install_java.sh
-  #               bash /opt/minecraft_servers/${replace(aws_s3_object.MinecraftServerProfileObject.key, ".zip", "")}/install_java.sh
-
-  #               EOF
 
   tags = {
     # Used to identify instances that are part of the Minecraft server infrastructure
@@ -639,3 +605,5 @@ resource "aws_instance" "MinecraftServerInstance" {
     "Description"       = var.MinecraftServerDescription
   }
 }
+
+
--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -14,18 +14,22 @@ aws s3 cp s3://${s3_bucket}/scripts/stop-server.py /home/ubuntu/stop-server.py
 chmod +x /home/ubuntu/stop-server.py
 
 # Pull the start service from S3
-aws s3 cp s3://${s3_bucket}/services/${service_key} /etc/systemd/system/start-server.service
+
+# FAILS
+sudo aws s3 cp s3://${s3_bucket}/${start_server_service} /etc/systemd/system/start-server.service
   
 # Enable the services to run at startup
 systemctl enable start-server.service
 systemctl start start-server.service
 
-# Download the Minecraft Profile from S3
-aws s3 cp s3://${s3_bucket}/profiles/${server_profile_key} /opt/minecraft_server_profiles/
+# Download the Minecraft minecraft_server_profile from S3
 
-# Setup the profile
-unzip /opt/minecraft_server_profiles/${server_profile_key} -d ${replace("/opt/minecraft_servers/${server_profile_key}", ".zip", "")}
+# FAILS
+sudo aws s3 cp s3://${s3_bucket}/${minecraft_server_profile} /opt/minecraft_server_profiles/
+
+# Setup the minecraft_server_profile
+unzip /opt/minecraft_server_profiles/${minecraft_server_profile} -d ${replace("/opt/minecraft_servers/${minecraft_server_profile}", ".zip", "")}
 
 # Run the java install script
-# chmod +x /opt/minecraft_servers/${replace(server_profile_key, ".zip", "")}/install_java.sh
-# bash /opt/minecraft_servers/${replace(server_profile_key, ".zip", "")}/install_java.sh
+chmod +x /opt/minecraft_servers/${replace(minecraft_server_profile, ".zip", "")}/install_java.sh
+bash /opt/minecraft_servers/${replace(minecraft_server_profile, ".zip", "")}/install_java.sh
